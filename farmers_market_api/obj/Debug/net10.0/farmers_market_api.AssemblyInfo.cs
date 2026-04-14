d0281afe90cdd2e25ad09056e1fb4254871b7408
Jan-Louis <20231441@ctucareer.co.za>
2026-04-14 10:38:25 +0200

    Update project references and add Swashbuckle.AspNetCore dependencies
    - Corrected project path in farmers_market_api.csproj.nuget.dgspec.json and related files.
    - Added Swashbuckle.AspNetCore version 10.1.7 as a dependency in project.assets.json.
    - Included Microsoft.Extensions.ApiDescription.Server version 10.0.0 and its related imports in project files.
    - Updated project.nuget.cache to reflect new package paths and dependencies.

--- farmers_market_api/obj/Debug/net10.0/farmers_market_api.AssemblyInfo.cs
+++ farmers_market_api/obj/Debug/net10.0/farmers_market_api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("farmers_market_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c8dbbdf8cccf232221f95347cb793d6f479d86e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb2e98109c2ae201dc2ba5d993c02bf17c58ec2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("farmers_market_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("farmers_market_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
